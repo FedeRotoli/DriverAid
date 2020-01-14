1e6096c8dad306a3057899afadd23eead162e28c
Federico Rotoli <federico.rotoli00@gmail.com>
2020-01-14 13:46:54 +0100

    Tanta roba

--- prova WatchKit Extension/ContentView.swift
+++ prova WatchKit Extension/ContentView.swift
@@ -9,81 +9,103 @@
 import SwiftUI
 import HealthKit
 
+
+struct PagerView<Content: View>: View {
+    let pageCount: Int
+    @Binding var currentIndex: Int
+    let content: Content
+
+    init(pageCount: Int, currentIndex: Binding<Int>, @ViewBuilder content: () -> Content) {
+        self.pageCount = pageCount
+        self._currentIndex = currentIndex
+        self.content = content()
+    }
+    
+    @GestureState private var translation: CGFloat = 0
+
+    var body: some View {
+        GeometryReader { geometry in
+            HStack(spacing: 0) {
+                self.content.frame(width: geometry.size.width)
+            }
+            .frame(width: geometry.size.width, alignment: .leading)
+            .offset(x: -CGFloat(self.currentIndex) * geometry.size.width)
+            .offset(x: self.translation)
+            .animation(.interactiveSpring())
+            .gesture(
+                DragGesture().updating(self.$translation) { value, state, _ in
+                    state = value.translation.width
+                }.onEnded { value in
+                    let offset = value.translation.width / geometry.size.width
+                    let newIndex = (CGFloat(self.currentIndex) - offset).rounded()
+                    self.currentIndex = min(max(Int(newIndex), 0), self.pageCount - 1)
+                }
+            )
+        }
+    }
+}
+
+
 struct ContentView: View {
+    @State private var currentPage = 0
+
+    var body: some View {
+        VStack{
+            PagerView(pageCount: 2, currentIndex: $currentPage) {
+                firstPage()
+                SettingsView()
+            }
+            HStack{
+                Circle()
+                    .frame(width: 8, height: 8)
+                    .foregroundColor(currentPage==1 ? Color.gray:Color.white)
+                Circle()
+                    .frame(width: 8, height: 8)
+                    .foregroundColor(currentPage==1 ? Color.white:Color.gray)
+            }
+        }
+
+    }
+}
+
+struct firstPage: View {
     @State private var showingSettings = false
     @State private var activeSecurity = false
     private var healthStore = HKHealthStore()
     private var timer: Timer?
     @State private var value = 0
-
+    
     var body: some View {
-        HStack{
-            
-            VStack{
-                Button(action:{
-                    self.activeSecurity.toggle()
-                    if self.activeSecurity{
-                        self.autorizeHealthKit()
-                        self.saveMockHeartData()
-                    }
-
-                }) {
-                    Image(systemName: "checkmark.shield").frame(width: 40, height: 40)
+        VStack{
+            Button(action:{
+                self.activeSecurity.toggle()
+                if self.activeSecurity{
+                    self.autorizeHealthKit()
+                    self.saveMockHeartData()
                 }
-                    .background(activeSecurity ? Color.green:Color.black)
-                    .animation(.easeInOut(duration: 0.5))
-                    .cornerRadius(10)
-                    .frame(width: 64, height: 64)
-                    .padding(.horizontal)
 
-                Text("Safety")
-                Text("BPM:  \(value)")
-            }.multilineTextAlignment(.center)
-
-            
-            VStack{
-                NavigationLink(destination: SettingsView()) {
-                    Image(systemName: "gear").frame(width: 40, height: 40)
-                }
+            }) {
+                Image(systemName: "checkmark.shield").frame(width: 40, height: 40)
+            }
+                .background(activeSecurity ? Color.green:Color.black)
+                .animation(.easeInOut(duration: 0.5))
+                .cornerRadius(10)
                 .frame(width: 64, height: 64)
                 .padding(.horizontal)
-                
-                Text("Settings")
-        
-            }.multilineTextAlignment(.center)
-            
-            
 
-        }
+            Text("Safety")
+            Text("BPM:  \(value)")
+        }.multilineTextAlignment(.center)
 
-        
     }
     
     func autorizeHealthKit() {
-//        if HKHealthStore.isHealthDataAvailable() {
-//
-//
-//            let healthKitTypesToRead: Set<HKObjectType> = [HKObjectType.quantityType(forIdentifier: .heartRate)!]
-//            let healthKitTypesToWrite: Set<HKSampleType> = []
-//
-//            if !HKHealthStore.isHealthDataAvailable(){
-//                print("Error occurred")
-//                return
-//            }
-//
-//            healthStore.requestAuthorization(toShare: healthKitTypesToWrite, read: healthKitTypesToRead) {(success, error) -> Void in
-//                print("Read Write Authorization succeded")
-//            }
-//        }
-        
-          let healthKitTypes: Set = [
-            HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!,
-            HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.distanceWalkingRunning)!
-          ]
+        let healthKitTypes: Set = [
+        HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!,
+        HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.distanceWalkingRunning)!]
 
-        healthStore.requestAuthorization(toShare: healthKitTypes,
-                                         read: healthKitTypes) { _, _ in }
-        
+        healthStore.requestAuthorization(toShare: healthKitTypes, read: healthKitTypes) { _, _ in }
+
     }
     
     func saveMockHeartData() {
@@ -102,19 +124,12 @@ struct ContentView: View {
         }
       })
     }
-    
-//    func startMockHeartData() {
-//        timer = Timer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "saveMockHeartData", userInfo: nil, repeats: true)
-//    }
-//
-//    func stopMockHeartData() {
-//      self.timer?.invalidate()
-//    }
-    
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
